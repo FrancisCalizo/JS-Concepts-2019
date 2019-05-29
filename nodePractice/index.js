181416c873fdce1b8aa8a2b5b471b22ff3adfc9a
Francis Calizo <francisacalizo@gmail.com>
2019-05-28 23:14:15 -0400

    Practice with NodejS Event Emitters

--- nodePractice/index.js
+++ nodePractice/index.js
@@ -1,9 +1,16 @@
-const person = require('./person');
-const Place = require('./place');
+// const person = require('./person');
+// const Place = require('./place');
+const Logger = require("./logger");
+// const place1 = new Place('San Francisco', 'California');
+// place1.location();
 
-const place1 = new Place('San Francisco', 'California');
-place1.location();
+// console.log(person);
 
-console.log(person);
+// console.log(`${__dirname}, ${__filename}`);
+const logger = new Logger();
 
-console.log(`${__dirname}, ${__filename}`);
+logger.on("message", data => console.log("Called Listener", data));
+
+logger.log("hello world");
+logger.log("hello hi");
+logger.log("hello whats up");
