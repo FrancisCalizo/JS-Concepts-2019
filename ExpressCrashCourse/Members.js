5f2bbc7d534f48fe12350d91ec91d9ef1146237c
Francis Calizo <francisacalizo@gmail.com>
2019-06-27 23:30:27 -0400

    Stop at 34Min Mark of Video and Add GitIgnore Files

---
+++ ExpressCrashCourse/Members.js
@@ -0,0 +1,24 @@
+// Uppercase filename because this is basically our "Model"
+
+const members = [
+  {
+    id: 1,
+    name: 'Francis Calizo',
+    email: 'FCalizo@yahoo.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    email: 'Jdoe@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Suzy Carmichael',
+    email: 'SCar@aol.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
