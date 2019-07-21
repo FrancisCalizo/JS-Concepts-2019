e14f88060c1bb9cb037991c9de599f170f78942d
Francis Calizo <francisacalizo@gmail.com>
2019-07-20 20:57:18 -0400

    Finish EasyHTTP Custom Library

--- CustomHTTPLibrary/app.js
+++ CustomHTTPLibrary/app.js
@@ -1,6 +1,61 @@
 const http = new easyHTTP();
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/postsd/1', function(err, post) {
-  console.log(err, post);
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// // Get Single Post
+http.get('https://jsonplaceholder.typicode.com/posts/5', function(err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// Create Data for POST Request
+const data = {
+  title: 'Custom Post',
+  body: ' This is a custom post'
+};
+
+// POST Request
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// PUT Request
+http.put('https://jsonplaceholder.typicode.com/posts/2', data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// // DELETE Post
+http.delete('https://jsonplaceholder.typicode.com/posts/3', function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
 });
--- CustomHTTPLibrary/easyHTTP.js
+++ CustomHTTPLibrary/easyHTTP.js
@@ -21,7 +21,45 @@ easyHTTP.prototype.get = function(url, callback) {
 };
 
 // Make an HTTP Post Request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let that = this;
+  this.http.onload = function() {
+    callback(null, that.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP Put Request
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let that = this;
+  this.http.onload = function() {
+    callback(null, that.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP Delete Request
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+
+  // Could Also Use an Arrow Function
+  let that = this;
+  this.http.onload = function() {
+    if (that.http.status == 200) {
+      // First Param is Error Catching
+      callback(null, 'Post Deleted');
+    } else {
+      callback(`Error: ${that.http.status}`);
+    }
+  };
+
+  this.http.send();
+};
