64d35521b9a5d6d8911591cdebc9bb0fa1c8d509
Francis Calizo <francisacalizo@gmail.com>
2019-07-22 21:25:17 -0400

    Complete GET Request using Promises and Fetch API in an asynchronous manner

--- CustomHTTPes6/app.js
+++ CustomHTTPes6/app.js
@@ -1,1 +1,6 @@
 const http = new EasyHTTP();
+
+http
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
--- CustomHTTPes6/easyhttp2.js
+++ CustomHTTPes6/easyhttp2.js
@@ -1,9 +1,13 @@
+// Using Promise with Fetch API so that Fetch is used asynchronously!
+
 class EasyHTTP {
   // Make an HTTP GET Request
   get(url) {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .error(err => console.log(err));
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
   }
 }
