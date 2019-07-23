52cb2667f61fc97afccd06caaaaa6ee60c4079f3
Francis Calizo <francisacalizo@gmail.com>
2019-07-22 22:12:08 -0400

    Create POST Request with Fetch

--- CustomHTTPes6/app.js
+++ CustomHTTPes6/app.js
@@ -1,6 +1,20 @@
 const http = new EasyHTTP();
 
+// Make GET Request
 http
   .get('https://jsonplaceholder.typicode.com/users')
-  .then(response => console.log(response))
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// Make POST Request
+const data = {
+  name: 'Nick Miller',
+  username: 'Illenium',
+  email: 'NMiller@sadbois.com'
+  // phone: '1-888-555-1234 x34234'
+};
+
+http
+  .post('https://jsonplaceholder.typicode.com/users', data)
+  .then(res => console.log(res))
   .catch(err => console.log(err));
--- CustomHTTPes6/easyhttp2.js
+++ CustomHTTPes6/easyhttp2.js
@@ -1,13 +1,29 @@
 // Using Promise with Fetch API so that Fetch is used asynchronously!
 
 class EasyHTTP {
-  // Make an HTTP GET Request
+  // Define a GET Request
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
         .then(res => res.json())
         .then(data => resolve(data))
         .catch(err => reject(err));
+    });
+  }
+
+  // Define a POST Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .then(err => reject(data));
     });
   }
 }
