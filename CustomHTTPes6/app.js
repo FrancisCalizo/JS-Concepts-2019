63f0d2fe205331d2cbabaecccba04ca5e0fd1515
Francis Calizo <francisacalizo@gmail.com>
2019-07-22 22:29:45 -0400

    Finish Custom HTTP Library using promises and Fetch

--- CustomHTTPes6/app.js
+++ CustomHTTPes6/app.js
@@ -10,11 +10,23 @@ http
 const data = {
   name: 'Nick Miller',
   username: 'Illenium',
-  email: 'NMiller@sadbois.com'
-  // phone: '1-888-555-1234 x34234'
+  email: 'NMiller@sadbois.com',
+  phone: '1-888-555-1234 x34234'
 };
 
 http
   .post('https://jsonplaceholder.typicode.com/users', data)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// Make a PUT Request
+http
+  .put('https://jsonplaceholder.typicode.com/users/3', data)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
+// Make a DELETE Request
+http
+  .delete('https://jsonplaceholder.typicode.com/users/2')
   .then(res => console.log(res))
   .catch(err => console.log(err));
--- CustomHTTPes6/easyhttp2.js
+++ CustomHTTPes6/easyhttp2.js
@@ -26,4 +26,35 @@ class EasyHTTP {
         .then(err => reject(data));
     });
   }
+
+  // Define a PUT Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .then(err => reject(data));
+    });
+  }
+
+  // Define a DELETE Request
+  delete(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => resolve('User has been deleted'))
+        .then(err => reject(data));
+    });
+  }
 }
