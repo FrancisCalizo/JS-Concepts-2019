65416511429d47be32c5f631266423890dc25587
Francis Calizo <francisacalizo@gmail.com>
2019-10-05 12:24:09 -0400

    Setup Contact Context and Reducer

---
+++ contact-keeper2/client/src/context/contact/ContactState.js
@@ -0,0 +1,66 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_ALERT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        name: 'Connor McGregor',
+        type: 'personal',
+        email: 'connor@ufc.com',
+        phone: '123-456-7890'
+      },
+      {
+        name: 'Harry White',
+        type: 'Personal',
+        email: 'harry@yahoo.com',
+        phone: '333-333-3333'
+      },
+      {
+        name: 'Ted Johnsoh',
+        type: 'Personal',
+        email: 'Ted@gmail.com',
+        phone: '222-222-2222'
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+
+  // Clear Current Contact
+
+  // Update Contact
+
+  // Filter Contacts
+
+  // Clear Filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
