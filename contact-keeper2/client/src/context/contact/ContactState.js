3b115a8ea3d71fdde7173f0621e758d9777f0a64
Francis Calizo <francisacalizo@gmail.com>
2019-10-08 23:30:03 -0400

    Implement DeleteContact

--- contact-keeper2/client/src/context/contact/ContactState.js
+++ contact-keeper2/client/src/context/contact/ContactState.js
@@ -50,7 +50,12 @@ const ContactState = props => {
     });
   };
   // Delete Contact
-
+  const deleteContact = id => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  };
   // Set Current Contact
 
   // Clear Current Contact
@@ -65,7 +70,8 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
