c8591991a1531139199aaca51a8459b95802fd0e
Francis Calizo <francisacalizo@gmail.com>
2019-10-31 23:09:23 -0400

    Start Load User and Set Token 3:35

--- contact-keeper2/client/src/context/auth/AuthState.js
+++ contact-keeper2/client/src/context/auth/AuthState.js
@@ -25,7 +25,20 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log('Load User');
+  const loadUser = async () => {
+    //@todo Load Token into Global Headers
+    try {
+      const res = await axios.get('api/auth');
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: AUTH_ERROR
+      });
+    }
+  };
 
   // Register User
   const register = async formData => {
